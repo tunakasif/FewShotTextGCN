a0ff6674383bc0ac8be1870d9ff5a1123713e053
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-02-24 23:09:48 +0300

    fix: `matthews` for cola

--- regeneration.sh
+++ regeneration.sh
@@ -12,14 +12,14 @@ r8 | r52 | mr | ohsumed | 20ng | cola | sst2)
 	dataset=$1
 
 	if [ "$dataset" == "cola" ]; then
-		is_mcc="--calc_mcc true"
+		is_mcc='--calc_mcc true'
 	else
-		is_mcc=""
+		is_mcc=''
 	fi
 
 	dev_splits=(0.01 0.80 0.90 0.95 0.99)
 	for item in "${dev_splits[@]}"; do
-		python main.py -c experiment_params/self_training.ini --experiment_name "$dataset" --path_to_train_set data/"$dataset"-train.tsv --path_to_test_set data/"$dataset"-test.tsv --tokenizer_type whitespace --language english --percentage_dev "$item" --num_seeds 1 --max_epochs 1000 "$is_mcc"
+		python main.py -c experiment_params/self_training.ini --experiment_name "$dataset" --path_to_train_set data/"$dataset"-train.tsv --path_to_test_set data/"$dataset"-test.tsv --tokenizer_type whitespace --language english --percentage_dev "$item" --num_seeds 1 --max_epochs 1000 $is_mcc
 	done
 	;;
 *)
